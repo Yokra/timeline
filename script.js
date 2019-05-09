3ef10cd93e960b3b2ceb2bfd5af8048e2ba269c6
yokra <yordanka_krasteva@mail.bg>
2019-05-09 18:38:16 +0200

    text apperars

--- script.js
+++ script.js
@@ -7,6 +7,56 @@ function loadSVG() {
     .then(res => res.text())
     .then(svgdata => {
       console.log(svgdata);
-      document.querySelector("body").insertAdjacentHTML("afterend", svgdata);
+      document.querySelector("main").insertAdjacentHTML("afterend", svgdata);
+      const textList = document.querySelectorAll(".textMovie");
+      console.log(textList);
+      textList.forEach(hideText);
+      const yearArrey = document.querySelectorAll(".click");
+      console.log(yearArrey);
+
+      yearArrey.forEach(year => {
+        console.log(year);
+        year.addEventListener("click", yearClicked);
+      });
     });
+}
+
+function hideText(text) {
+  text.classList.add("hide");
+  console.log(text);
+}
+
+function yearClicked(event) {
+  console.log(event.target.innerHTML === "1966");
+
+  if (event.target.innerHTML === "1966") {
+    document.querySelector("#BatmanTheMovie").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "1989") {
+    document.querySelector("#Batman").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "1992") {
+    document.querySelector("#BatmanReturns").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "1995") {
+    document.querySelector("#BatmanForever").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "1997") {
+    document.querySelector("#BatmanAndRobin").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "2005") {
+    document.querySelector("#BatmanBegins").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "2008") {
+    document.querySelector("#TheDarkNight").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "2012") {
+    document.querySelector("#TheDarkKnightRises").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "2016") {
+    document.querySelector("#BatmanvSuperman").classList.remove("hide");
+  }
+  if (event.target.innerHTML === "2017") {
+    document.querySelector("#JusticeLeague").classList.remove("hide");
+  }
 }
